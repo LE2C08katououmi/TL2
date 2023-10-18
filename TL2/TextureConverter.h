aeafe08600ec5a7609d4e049d27b78117edf15f1
大海 加藤 <oumikato0310@outlook.jp>
2023-10-18 16:44:55 +0900

    02_05完成

--- TL2/TextureConverter.h
+++ TL2/TextureConverter.h
@@ -1,8 +1,23 @@
 #pragma once
 #include <string>
 #include <DirectXTex.h>
+#include <DirectXTex.h>
 class TextureConverter
 {
+private:
+	// 画像の情報
+	DirectX::TexMetadata metadata_;
+	// 画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+
+private:
+	// ディレクトリパス
+	std::wstring directoryPath_;
+	// ファイル名
+	std::wstring fileName_;
+	// ファイル拡張子
+	std::wstring fileExt_;
+
 public:
 	/// <summary>
 	/// テクスチャをWICからDDSに変換する
@@ -17,6 +32,17 @@ private:
 	/// </summary>
 	/// <param name="filePath"></param>
 	void LoadWICTextureFromFile(const std::string& filePath);
+
+	/// <summary>
+	/// フォルダパスとファイル名を分離する
+	/// </summary>
+	/// <param name="filePath"></param>
+	void SeparateFilePath(const std::wstring& filePath);
+
+	/// <summary>
+	/// DDSテクスチャとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
 
 	/// <summary>
 	/// マルチバイト文字列をワイド文字列に変換
